ae8117c166c88fe48494d4d54a91ab5180aaaf76
Savenok <Romasav2017@gmail.com>
2022-12-03 17:28:50 +0300

    5

---
+++ DzEx4.py
@@ -0,0 +1,70 @@
+from random import randint
+from random import choice
+import random
+'''
+4 Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+(значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+*Пример:*
+
+- k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+'''
+
+
+a = 0
+b = 0
+c = 0
+k = int(input("Enter cof k: "))
+
+
+file = open('Ex4.txt', "w")
+#(f'{a}x ** {k} + {b}x + {c} = 0'), (f'{a}x ** {k} + {c} = 0'), (f'{a}x ** {k} = 0')
+lst = [1, 2, 3]
+random_write = random.choice(lst)
+if random_write == lst[1]:
+    a = randint(1, 100)
+    b = randint(1, 100)
+    c = randint(1, 100)
+    result1 = f'{a}x ** {k} + {b}x + {c} = 0'
+elif random_write == lst[2]:
+    a = randint(1, 100)
+    c = randint(1, 100)
+    result1 =  f'{a}x ** {k} + {b}x + {c} = 0'
+else:
+    a = randint(1, 100)
+    result1 = f'{a}x ** {k} + {b}x + {c} = 0'
+file.write(result1)
+
+file.close()
+
+
+#(f'{a1}x ** {k1} - {b1}x + {c1} = 0')
+
+k1 = int(input("Enter cof k1: "))
+
+a1 = 0
+b1 = 0
+c1 = 0
+
+file2 = open('Ex5.txt', "w")
+#(f'{a1}x ** {k1} + {b1}x + {c1} = 0') , (f'{a1}x ** {k1} + {c1} = 0') , (f'{a1}x ** {k1} = 0')
+lst2 = [1, 2, 3]
+random_write2 = random.choice(lst2)
+
+if random_write2 == lst2[1]:
+    a1 = randint(1, 100)
+    b1 = randint(1, 100)
+    c1 = randint(1, 100)
+    result = f'{a1}x ** {k1} + {b1}x + {c1} = 0'
+elif random_write2 == lst2[2]:
+    a1 = randint(1, 100)
+    c1 = randint(1, 100)
+    result = f'{a1}x ** {k1} + {c1} = 0'
+else:
+    a1 = randint(1, 100)
+    result = f'{a1}x ** {k1} = 0'
+
+file2.write(result)
+
+file2.close()
+
---
+++ DzEx5.py
@@ -0,0 +1,30 @@
+import random
+from random import randint
+import DzEx4 as d
+'''
+5 Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+Коэффициенты могут быть как положительными, так и отрицательными. Степени многочленов могут отличаться.
+'''
+
+
+
+
+file4 = open('Ex4.txt', "r")
+
+file5 = open('Ex5.txt', "r")
+
+print(file4.readline())
+print(file5.readline())
+
+file4.close()
+file5.close()
+
+
+
+file_result = open('result.txt', "w")
+
+result = f'{d.a + d.a1}x ** {d.k + d.k1} +  {d.b + d.b1}x +  {d.c + d.c1} = 0'
+
+file_result.write(result)
+
+file_result.close()
