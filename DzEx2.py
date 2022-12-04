01d4b440e6f59b4d5d4e704c3f01f35cc6bb5e0d
Savenok <Romasav2017@gmail.com>
2022-12-04 09:23:29 +0300

    2.2

--- DzEx2.py
+++ DzEx2.py
@@ -6,4 +6,28 @@
 - при N=236     ->        [2, 2, 59]
 
 '''
+number = int(input("enter some number: "))
+def simple_numbers(n):
+    lst = []
+    def fk(n):
+        if n != 1 and n != 2:
+            for i in range(2, n):
+                if i % i == 0 and i % 1 == 0 and i % (i+1) != 0:
+                    if n % i == 0:
+                        lst.append(i)
+                        if n == 1:
+                            break
+                        else:
+                            n = n / i
+                            fk(int(n))
 
+                    else:
+                        continue
+                else:
+                    continue
+        elif n == 1:
+            print(lst)
+    fk(number)
+
+
+simple_numbers(number)
