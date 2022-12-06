c6466ee6439015876830502b34dc122b928e21e6
Savenok <Romasav2017@gmail.com>
2022-12-06 18:17:14 +0300

    Переделка

--- DzEx2.py
+++ DzEx2.py
@@ -4,30 +4,19 @@
 *Пример*
 
 - при N=236     ->        [2, 2, 59]
-
 '''
+
 number = int(input("enter some number: "))
-def simple_numbers(n):
+def Factor(n):
     lst = []
-    def fk(n):
-        if n != 1 and n != 2:
-            for i in range(2, n):
-                if i % i == 0 and i % 1 == 0 and i % (i+1) != 0:
-                    if n % i == 0:
-                        lst.append(i)
-                        if n == 1:
-                            break
-                        else:
-                            n = n / i
-                            fk(int(n))
-
-                    else:
-                        continue
-                else:
-                    continue
-        elif n == 1:
-            print(lst)
-    fk(number)
-
-
-simple_numbers(number)
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            lst.append(d)
+            n = n // d
+        else:
+            d += 1
+    if n > 1:
+        lst.append(n)
+    print(lst)
+Factor(number)
