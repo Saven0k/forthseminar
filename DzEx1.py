d5a63a3750ed3fef566719a0320b33cb979f26cb
Savenok <Romasav2017@gmail.com>
2022-12-06 18:13:55 +0300

    Переделка

--- DzEx1.py
+++ DzEx1.py
@@ -5,7 +5,7 @@
 
 - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-'''
+
 #Формула выслениә пи((2**0.5) + (3 ** 0.5))
 
 def find_pi():
@@ -13,11 +13,21 @@ def find_pi():
     d2 = ''
     d3 = []
     for i in range(len(d)):
-        d3.append(d[i].replace('.' , '0'))
+        d3.append(d[i].replace('.', '0'))
     pi = round(((2**0.5) + (3 ** 0.5)), (len(d) - 2))
     print(f' при $d =  {d2.join(d)}  π =  {pi}')
-
-
-find_pi()
+'''
+def pi():
+    count = 0
+    d = float(input("Enter the value of the number d: "))
+    while d < 1:
+        d = d * 10
+        count += 1
+    k = 1
+    x = 0
+    for k in range(1, 1000000):
+        x = x + 4 * ((-1) ** (k + 1)) / (2 * k - 1)
+    print(round(x, count))
+pi()
 
 #Честно скажу задание вообще не понял делал по примеру)
